5dd08db9d97436f2b18a325e0407ca02cfc89eaf
anso33 <hyeon131812@gmail.com>
2022-09-08 16:04:35 +0900

    [정렬] 0902 - update

--- 0902-정렬/1946.cpp
+++ 0902-정렬/1946.cpp
@@ -9,16 +9,7 @@
 
 using namespace	std;
 
-bool	compareFirst(pair<int,int> a, pair<int,int> b) {
-	return (a.first < b.first);
-}
-
-int	main () {
-
-	int test_case;
-	cin >> test_case;
-	vector<int> pass;
-	pass.assign(test_case, 1);
+void	passNumber(int test_case) {
 
 	int n;
 	for (int i = 0; i < test_case; i++) {
@@ -28,18 +19,25 @@ int	main () {
 			cin >> rank[k].first >> rank[k].second;
 		}
 
-		sort(rank.begin(), rank.end(), compareFirst);
+		sort(rank.begin(), rank.end()); // pair로 되어 있다면 첫번째 값을 기준으로 오름차순으로 정렬하는 것이 default이므로 굳이 함수를 만들 필요없다. 
 
 		int	limit = rank[0].second;
+		int pass = 1;
 		for (int k = 1; k < n; k++) {
 			if (rank[k].second < limit) {
-				pass[i]++;
+				pass++;
 				limit = rank[k].second; // 두번째의 최소 랭크를 계속 갱신하여 갈수록 최소 랭크 기준이 높아지도록 한다. 
 			}
 		}
+		cout << pass << "\n";
 	}
-	for (int i = 0; i < test_case; i++) {
-		cout << pass[i] << "\n";
-	}
+}
+
+int	main () {
+
+	int test_case;
+	cin >> test_case;
+
+	passNumber(test_case);
 	return (0);
 }
