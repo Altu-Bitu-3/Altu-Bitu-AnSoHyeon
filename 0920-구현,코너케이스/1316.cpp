4c829fb714ffa875f2ed2a27fbe333c473cab7d1
anso33 <hyeon131812@gmail.com>
2022-09-26 20:28:36 +0900

    [구현 코너케이스] 0924 - 1316 refactoring

--- 0920-구현,코너케이스/1316.cpp
+++ 0920-구현,코너케이스/1316.cpp
@@ -4,25 +4,29 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace	std;
 
 //이면 1, 아니면 0을 반환
-int	isGroupWord(string word) {
+bool	isGroupWord(string word) {
 
+	vector <bool> alpha(26, false);
 	string tmp;
-	tmp.push_back(word.front());
+	tmp.push_back(word[0]);
+	alpha[word[0] - 'a'] = true;
 	for (int i = 1; i < word.size(); i++) {
 		if (word[i] == tmp.back())	//같은 문자가 연속되고 있으면 pass
 			tmp.push_back(word[i]);
 		else {
-			if (tmp.find(word[i]) == tmp.npos) // 만일 지금 찾는 문자가 앞에 없으면 pass
+			if (!alpha[word[i] - 'a']) // 만일 지금 찾는 문자가 앞에 없으면 pass
 				tmp.push_back(word[i]);
 			else
-				return 0;
+				return false;
 		}
+		alpha[word[i] - 'a'] = true;
 	}
-	return 1;
+	return true;
 }
 
 int	main () {
